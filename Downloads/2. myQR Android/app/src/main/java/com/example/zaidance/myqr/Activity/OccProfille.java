e02b8280dd2e52aa36b53a31a0733a31acd0dd65
zaian <zaidan>
2019-11-06 17:28:57 +0700

    commit

--- Downloads/2. myQR Android/app/src/main/java/com/example/zaidance/myqr/Activity/OccProfille.java
+++ Downloads/2. myQR Android/app/src/main/java/com/example/zaidance/myqr/Activity/OccProfille.java
@@ -1,4 +1,21 @@
 package com.example.zaidance.myqr.Activity;
 
-public class OccProfille {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.example.zaidance.myqr.R;
+
+public class OccProfille extends AppCompatActivity {
+    //labeling your debugging log
+    private static final String TAG = "OccProfille";
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_occprofille);
+        Log.d(TAG, "onCreate: started.");
+    }
 }
